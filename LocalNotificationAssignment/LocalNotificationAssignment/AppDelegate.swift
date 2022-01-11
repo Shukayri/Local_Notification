e46a84cdb8a17f47c217e5136618c22e34202d0f
Shukayri <abdulmajeed.shukayri@students.cau.edu>
2022-01-10 23:01:43 -0800

    fixed

--- LocalNotificationAssignment/LocalNotificationAssignment/AppDelegate.swift
+++ LocalNotificationAssignment/LocalNotificationAssignment/AppDelegate.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate  {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UNUserNotificationCenter.current().delegate = self
+                      UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+                          if granted {
+                              print("User gave permissions for local notifications")
+                          }
+                      }
         return true
     }
 
